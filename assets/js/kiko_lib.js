b46323d7fdda6a6c83559a656bfb45438d8deccf
André Tonic <andre_tonic@yahoo.fr>
2021-07-09 12:15:04 +0200

    html11

--- assets/js/kiko_lib.js
+++ assets/js/kiko_lib.js
@@ -12,7 +12,12 @@ let M_PI = Math.acos(-1.0);
 // TO DO - coeur du système (#1)
 // const prix_m2 = require('../data/prix_maisons_m2.json');    //(#2)
 // const lat_long_CNPE = require('../data/centrales.json');    //(#3)
-fetch("assets/data/centrales.json") .then(response => {return response.json();}) .then(data => {const lat_long_CNPE = data;});
+// fetch("assets/data/centrales.json") .then(response => {return response.json();}) .then(data => {const lat_long_CNPE = data;});
+async function fetchText() {
+    let response = await fetch("assets/data/centrales.json");
+    let data = await response.text();
+    const lat_long_CNPE = data;
+}
 console.log(lat_long_CNPE);
 
 // This function converts decimal degrees to radians
