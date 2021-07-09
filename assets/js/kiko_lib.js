03b8a91551eb67e16d4e3bd63e2c8f9eb6f47b56
André Tonic <andre_tonic@yahoo.fr>
2021-07-09 12:24:15 +0200

    html13

--- assets/js/kiko_lib.js
+++ assets/js/kiko_lib.js
@@ -15,9 +15,10 @@ let M_PI = Math.acos(-1.0);
 // fetch("assets/data/centrales.json") .then(response => {return response.json();}) .then(data => {const lat_long_CNPE = data;});
 async function fetchText() {
     let response = await fetch("assets/data/centrales.json");
-    return await response.text();
+    return await response.json();
 
 }
+
 const lat_long_CNPE = fetchText();
 console.log(lat_long_CNPE);
 
